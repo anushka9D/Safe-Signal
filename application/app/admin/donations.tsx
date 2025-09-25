30b8c9e8d1fce30abb5826b732edac30b201bd7f
Munasinghe123 <jsan39196.com>
2025-09-25 15:18:01 +0530

    FIx:Admin Dashboard

---
+++ application/app/admin/donations.tsx
@@ -0,0 +1,11 @@
+
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native';
+
+export default function Donations() {
+  return (
+    <SafeAreaView className="flex-1 bg-[#0b1220] p-5">
+      <Text className="text-white text-xl font-bold">Donations</Text>
+    </SafeAreaView>
+  );
+}
